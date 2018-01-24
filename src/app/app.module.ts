85e6eefac45e117c6ccacc6a452b301342740118
NITU <suraj.mehta@ongraph.in>
2018-01-24 11:30:21 +0530

    chat room

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ChatRoomComponent } from './chat-room/chat-room.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatRoomComponent
   ],
   imports: [
     BrowserModule
