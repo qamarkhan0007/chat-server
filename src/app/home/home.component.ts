9b957ce1ded7a8f0876556d563b08f972b355b9d
Qamar Khan <qamar.khan@ongraph.ca>
2018-01-24 12:39:04 +0530

    config

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Socket } from 'ng-socket-io';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  nickname = '';
 
-  constructor() { }
+  constructor(private socket: Socket, private router: Router) { }
 
   ngOnInit() {
   }
+  joinChat() {
+    this.socket.connect();
+    this.socket.emit('set-nickname', this.nickname);
+    this.router.navigate(['/chat-room']);
+  }
+
 
 }
